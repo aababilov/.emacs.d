346ea92affd61719cd43d35286e3d045ae8b2173
Alessio Ababilov <ilovegnulinux@gmail.com>
2013-05-26 10:44:45 +0300

    Initial commit

---
+++ custom.el
@@ -0,0 +1,46 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auto-compression-mode t nil (jka-compr))
+ '(c++-mode-hook (quote (hs-minor-mode)) t)
+ '(c-basic-offset (quote set-from-style))
+ '(c-comment-only-line-offset (quote set-from-style))
+ '(c-default-style "k&r")
+ '(c-mode-common-hook nil t)
+ '(c-mode-hook (quote (hs-minor-mode)) t)
+ '(c-special-indent-hook nil)
+ '(c-style-variables-are-local-p nil)
+ '(case-fold-search t)
+ '(column-number-mode t)
+ '(compile-command "make -k ")
+ '(current-language-environment "UTF-8")
+ '(default-input-method "russian-computer")
+ '(ecb-options-version "2.40")
+ '(ecb-source-path (quote ("~/job/cvs/")))
+ '(ecb-tip-of-the-day nil)
+ '(global-font-lock-mode t nil (font-lock))
+ '(indent-tabs-mode nil)
+ '(inferior-lisp-program "clisp" t)
+ '(iso-ascii-convenient nil)
+ '(ispell-local-dictionary "uk")
+ '(latex-run-command "pdflatex")
+ '(ps-print-color-p (quote black-white))
+ '(safe-local-variable-values (quote ((asm-comment-char . 35))))
+ '(save-place t nil (saveplace))
+ '(scroll-bar-mode (quote right))
+ '(session-save-file "/home/alessio/.emacs.d/session")
+ '(show-paren-mode t nil (paren))
+ '(terminal-escape-char 30)
+ '(tex-dvi-view-command "seepdf")
+ '(tex-run-command "pdftex")
+ '(text-mode-hook (quote (turn-on-auto-fill text-mode-hook-identify)))
+ '(transient-mark-mode t)
+ '(x-select-enable-clipboard t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ init.el
@@ -0,0 +1,166 @@
+;; Turn off mouse interface early in startup to avoid momentary display
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; No splash screen please ... jeez
+(setq inhibit-startup-message t)
+
+;; Set path to dependencies
+(setq site-lisp-dir
+      (expand-file-name "site-lisp" user-emacs-directory))
+
+;; Set up load path
+(add-to-list 'load-path user-emacs-directory)
+(add-to-list 'load-path site-lisp-dir)
+
+;; Keep emacs Custom-settings in separate file
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
+
+(server-start)
+
+(put 'set-goal-column 'disabled nil)
+
+(set-default-font "-misc-fixed-medium-r-*-*-15-*-*-*-*-*-*")
+
+;; Scrolling and indenting
+
+(setq scroll-conservatively 50)
+(setq scroll-up-agressively 0)
+(setq scroll-down-agressively 0)
+(global-set-key "\r" 'newline-and-indent)
+
+
+;; tabbar
+(if (eq window-system 'x)
+    (progn 
+      (require 'tabbar)
+      (tabbar-mode t)
+      (global-set-key (kbd "\e]") 'tabbar-forward)
+      (global-set-key (kbd "\e[") 'tabbar-backward)
+      )
+  )
+
+
+;; session
+(require 'session)
+(add-hook 'after-init-hook 'session-initialize)
+
+
+;; cedet
+;;(setq semantic-load-turn-useful-things-on t)
+;;(require 'cedet)
+;;(global-set-key (kbd "\C-c SPC") 'semantic-ia-complete-symbol)
+(require 'ecb)
+(global-set-key (kbd "\e\ee") 'ecb-activate)
+
+
+;; Compilation
+
+;; general compile function call "make all"
+(defun user-save-and-make()
+  "save and call shell command: make all"
+  (interactive)
+  (save-buffer)
+  (compile compile-command)
+  (message (concat compile-command " executed!")))
+
+;; general compile function call "make all"
+(defun user-save-and-make-clean ()
+  "save and call shell command: make all"
+  (interactive)
+  (save-buffer)
+  (compile "make -k clean")
+  (message "make -k clean executed!"))
+
+;; general compile function call "make all"
+(defun user-save-and-make-build ()
+  "save and call shell command: make all"
+  (interactive)
+  (save-buffer)
+  (compile "make -k clean all")
+  (message "make -k clean all executed!"))
+
+;; compilation window shall scroll down
+(setq compilation-scroll-output 1)
+
+(global-set-key (kbd "\e\em") 'user-save-and-make)
+(global-set-key [f9] 'user-save-and-make)
+(global-set-key (kbd "\e\ec") 'user-save-and-make-clean)
+(global-set-key (kbd "\e\eb") 'user-save-and-make-build)
+(global-set-key (kbd "\e\ek") 'kill-compilation)
+
+
+;; C/C++
+(defun linux-c-mode-hook()
+  "C mode with adjusted defaults for use with the Linux kernel."
+  (c-set-style "K&R")
+  (setq tab-width 8)
+  (setq indent-tabs-mode t)
+  (setq c-basic-offset 8)
+  (make-variable-buffer-local 'c-macro-cppflags)
+)
+
+(add-hook 'c-mode-common-hook 'linux-c-mode-hook)
+
+(defvar pkg-config-cflags)
+
+(setq pkg-config-cflags "`pkg-config --cflags dbus-1 dbus-glib-1 glib-2.0 gtk+-2.0`")
+
+(defun c++-cpp-hook()
+  "C++ mode with nice preprocessor."
+  (setq c-macro-cppflags (concat pkg-config-cflags " -x c++")))
+
+(add-hook 'c++-mode-hook 'c++-cpp-hook)
+
+(defun c-cpp-hook ()
+  "C mode with nice preprocessor."
+  (setq c-macro-cppflags pkg-config-cflags))
+
+(add-hook 'c-mode-hook 'c-cpp-hook)
+
+
+;; ASM
+(defun asm-mode-set-comment-hook()
+  (setq asm-comment-char 35))
+
+
+;; JSON
+(add-to-list 'auto-mode-alist '("\\.json$" . javascript-mode))
+
+
+;; TEX
+(add-hook 'tex-mode-hook (function (lambda () (setq ispell-parser 'tex))))
+
+
+;; Python
+
+(when (load "flymake" t)
+      (defun flymake-pylint-init ()
+        (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                           'flymake-create-temp-inplace))
+               (local-file (file-relative-name
+                            temp-file
+                            (file-name-directory buffer-file-name))))
+          (list "epylint" (list local-file))))
+      (add-to-list 'flymake-allowed-file-name-masks
+                   '("\\.py\\'" flymake-pylint-init)))
+
+(defun my-flymake-show-help ()
+  (when (get-char-property (point) 'flymake-overlay)
+    (let ((help (get-char-property (point) 'help-echo)))
+      (if help (message "%s" help)))))
+
+(add-hook 'post-command-hook 'my-flymake-show-help)
+;(add-hook 'python-mode-hook 'flymake-mode)
+
+;; Jinja
+(autoload 'jinja2-mode "jinja2-mode" nil t)
+(add-to-list 'auto-mode-alist '("\.jinja2$" . jinja2-mode))
+(add-to-list 'auto-mode-alist '("templates/.*\.html$" . jinja2-mode))
+
+;; Lisp
+(add-to-list  'load-path  "/usr/share/emacs/site-lisp/slime/")
+(require 'slime)
+(slime-setup)
